d071c486a3a3f741166bac80a206f8971d1c0de9
Rohit Jangid <73385164+rohitjangid666@users.noreply.github.com>
2025-02-22 22:52:44 +0530

    style: main layout styling change

--- app/layouts/main.tsx
+++ app/layouts/main.tsx
@@ -4,16 +4,20 @@ const MainLayout = () => {
   const pathname = useLocation().pathname;
 
   return (
-    <div className='flex items-center justify-center pt-16 pb-4'>
-      <div className='max-w-7xl w-full space-y-6 px-4'>
-        <h1 className='capitalize text-5xl font-bold text-center'>
-          {pathname.replaceAll('/', '').replaceAll('-', ' ')}
-        </h1>
-        <div className='rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4'>
-          <Outlet />
+    <main className='flex items-center justify-center pt-16 pb-4'>
+      <div className='flex-1 flex flex-col items-center gap-16 min-h-0'>
+        <header className='flex flex-col items-center gap-9'>
+          <h1 className='capitalize text-2xl font-bold text-center leading-none'>
+            {pathname.replaceAll('/', '').replaceAll('-', ' ')}
+          </h1>
+        </header>
+        <div className='max-w-7xl w-full space-y-6 px-4'>
+          <div className='rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4'>
+            <Outlet />
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
